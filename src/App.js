8f513ae2ebdb0163cf7b2aa5d2caeb7d5ef4b10e
yakup <lvashy86@gmail.com>
2022-09-22 20:26:59 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from "react"
+import userImg from "../src/images/user.png";
+function App() {
+    const [contact, setContact] = React.useState({
+        firstName: "John",
+        lastName: "Doe",
+        phone: "+1 (719) 555-1212",
+        email: "itsmyrealname@example.com",
+        isFavorite: false
+    })
+    
+    let starIcon = contact.isFavorite ? "star-filled.png" : "star-empty.png"
+    
+    function toggleFavorite() {
+        setContact(prevContact => ({
+            ...prevContact,
+            isFavorite: !prevContact.isFavorite
+        }))
+    }
+    
+    return (
+        <main>
+            <article className="card">
+                <img src={userImg}  alt="useruser" className="card--image"/>
+                <div className="card--info">
+                    <img 
+                        src={require(`../src/images/${starIcon}`)}  
+                        alt="star"
+                        className="card--favorite"
+                        onClick={toggleFavorite}
+                        
+                    />
+                    <h2 className="card--name">
+                        {contact.firstName} {contact.lastName}
+                    </h2>
+                    <p className="card--contact">{contact.phone}</p>
+                    <p className="card--contact">{contact.email}</p>
+                </div>
+                
+            </article>
+        </main>
+    )
+}
+export default App;
